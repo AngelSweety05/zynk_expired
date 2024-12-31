dd990085c661a07ddbd9fb65c812ceace1b4852b
AngelSweety05 <moviesadda.pro@gmail.com>
2024-12-31 08:37:31 -0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,15 +29,15 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
-        # try:
-        #     db_channel = await self.get_chat(CHANNEL_ID)
-        #     self.db_channel = db_channel
-        #     test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
-        #     await test.delete()
-        # except Exception as e:
-        #     self.LOGGER(__name__).warning(e)
-        #     self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-        #     self.LOGGER(__name__).info("\nBot Stopped. Join channel https://t.me/LazyDeveloper for support")
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join channel https://t.me/LazyDeveloper for support")
             # sys.exit() #if bot is admin & you are getting admin issue again and again then u can also remove this line of code 
 
         self.set_parse_mode(ParseMode.HTML)
